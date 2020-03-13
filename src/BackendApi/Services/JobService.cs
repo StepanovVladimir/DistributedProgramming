9032695e96f530426208716420ad6e7d8b841022
StepanovVladimir <vovan.number.one98@gmail.com>
2020-03-13 17:16:26 +0300

    Добавил работу с Логгером через Nats

--- src/BackendApi/Services/JobService.cs
+++ src/BackendApi/Services/JobService.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Grpc.Core;
 using Microsoft.Extensions.Logging;
+using NATS.Client;
 using StackExchange.Redis;
 
 namespace BackendApi.Services
 {
     public class JobService : Job.JobBase
     {
-        //private readonly static Dictionary<string, string> _jobs = new Dictionary<string, string>();
         private readonly static IDatabase _db = ConnectionMultiplexer.Connect("localhost").GetDatabase();
         private readonly ILogger<JobService> _logger;
 
@@ -21,14 +21,20 @@ namespace BackendApi.Services
         public override Task<RegisterResponse> Register(RegisterRequest request, ServerCallContext context)
         {
             string id = Guid.NewGuid().ToString();
+
+            _db.StringSet(id, request.Description);
+
+            string natsConnectionString = "nats://127.0.0.1";
+            var greeterService = new GreeterService();
+            using (IConnection connection = new ConnectionFactory().CreateConnection(natsConnectionString))
+            {
+                greeterService.Run(connection, id);
+            }
+
             var resp = new RegisterResponse
             {
                 Id = id
             };
-
-            //_jobs[id] = request.Description;
-            _db.StringSet(id, request.Description);
-
             return Task.FromResult(resp);
         }
     }
---
+++ src/Logger/SubscriberService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NATS.Client;
+using NATS.Client.Rx;
+using NATS.Client.Rx.Ops;
+using StackExchange.Redis;
+
+namespace Logger
+{
+    class SubscriberService
+    {
+        private readonly static IDatabase _db = ConnectionMultiplexer.Connect("localhost").GetDatabase();
+
+        public void Run(IConnection connection)
+        {
+            var greetings = connection.Observe("greeter")
+                    .Where(m => m.Data?.Any() == true)
+                    .Select(m => Encoding.Default.GetString(m.Data));
+
+            greetings.Subscribe(msg =>
+            {
+                string description = _db.StringGet(msg);
+                Console.WriteLine($"id: {msg}; description: {description}");
+            });
+        }
+    }
+}
